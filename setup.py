91b54639a6bc862ce6ff5fccaf263cb36a94a2f4
Amogh <114276750+amoghbharadwaj21@users.noreply.github.com>
2024-07-19 22:59:31 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file:str)->List[str]:
+    '''Read the requirements file and return the list of requirements'''
+    
+    requirements=[]
+    with open(file) as file_obj:
+        requirements= file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+            
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Amogh',
+    author_email='amoghbharadwaj21@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
